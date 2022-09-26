07bc947102a63008b60eda6650d898eda73bad11
trungswap <85089534+trungswap@users.noreply.github.com>
2022-09-26 16:59:28 +0700

    Add files via upload

---
+++ gan.java
@@ -0,0 +1,15 @@
+public class gan // toán  tử gán
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+
+        int b = a-1;
+
+        int c = b = 29;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b );
+        System.out.println("c = " + c);
+
+    }
+}
